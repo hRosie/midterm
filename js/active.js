80ccae3ccb1f4f9f70621b9fa130101453e4fdf2
Rosiel0ver <hieuRosie1102@gmail.com>
2021-11-04 13:19:42 +0700

    master

--- js/active.js
+++ js/active.js
@@ -99,33 +99,14 @@
         $.preventDefault();
     });
 
-    // :: 7.0 Search Form Active Code
-    $(".searchBtn").on('click', function () {
-        $(".search-hidden-form").toggleClass("search-form-open");
-    });
-
-    // :: 8.0 Search Form Active Code
-    $("#pattern-switcher").on('click', function () {
-        $("body").toggleClass("bg-pattern");
-    });
-    $("#patter-close").on('click', function () {
-        $(this).hide("slow");
-        $("#pattern-switcher").addClass("pattern-remove");
-    });
-
-    // :: 9.0 wow Active Code
-    if ($.fn.init) {
-        new WOW().init();
-    }
-
-    // :: 10.0 matchHeight Active JS
+    // matchHeight Active JS
     if ($.fn.matchHeight) {
         $('.item').matchHeight();
     }
 
     var $window = $(window);
 
-    // :: 11.0 Preloader active code
+    // Preloader active code
     $window.on('load', function () {
         $('#preloader').fadeOut('slow', function () {
             $(this).remove();
